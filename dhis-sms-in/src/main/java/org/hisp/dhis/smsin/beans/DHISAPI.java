062f453f80e5a41c79ee77f6da0867ebe40c6685
Mobilars <lars@roland.bz>
2014-02-03 14:22:50 +0100

    Added SSL support. Ignores the cert in case its self signed.

--- dhis-sms-in/src/main/java/org/hisp/dhis/smsin/beans/DHISAPI.java
+++ dhis-sms-in/src/main/java/org/hisp/dhis/smsin/beans/DHISAPI.java
@@ -10,6 +10,14 @@ import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Set;
+import java.net.MalformedURLException;
+import java.security.cert.Certificate;
+import java.io.*;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLPeerUnverifiedException;
+import javax.net.ssl.SSLSession;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -18,11 +26,11 @@ import org.hisp.dhis.smsin.utils.Base64;
 public class DHISAPI {
 
 	protected final Log logger = LogFactory.getLog(getClass());
-	
+
 	private String username;
-	private String password;	
+	private String password;
 	private String dhisUrl;
-	
+
 	public String getUsername() {
 		return username;
 	}
@@ -47,123 +55,286 @@ public class DHISAPI {
 		this.dhisUrl = dhisUrl;
 	}
 
-	
-	public boolean submitXML(String username, String password, String argUrl, String requestXml)
-	{
-		try
-		{
+	public boolean submitXML(String username, String password, String argUrl,
+			String requestXml) {
+		try {
 			String authStr = username + ":" + password;
-	        String authEncoded = Base64.encodeBytes(authStr.getBytes());
+			String authEncoded = Base64.encodeBytes(authStr.getBytes());
 
-		    URL url = new URL( argUrl );
-		    URLConnection con = url.openConnection();
-	        con.setRequestProperty("Authorization", "Basic " + authEncoded);
-		    
-		    // specify that we will send output and accept input
-		    con.setDoInput(true);
-		    con.setDoOutput(true);
+			URL url = new URL(argUrl);
+			URLConnection con = url.openConnection();
+			con.setRequestProperty("Authorization", "Basic " + authEncoded);
 
-		    con.setConnectTimeout( 20000 );  // long timeout, but not infinite
-		    con.setReadTimeout( 20000 );
+			// specify that we will send output and accept input
+			con.setDoInput(true);
+			con.setDoOutput(true);
 
-		    con.setUseCaches (false);
-		    con.setDefaultUseCaches (false);
+			con.setConnectTimeout(20000); // long timeout, but not infinite
+			con.setReadTimeout(20000);
 
-		    // tell the web server what we are sending
-		    con.setRequestProperty ( "Content-Type", "application/xml" );
+			con.setUseCaches(false);
+			con.setDefaultUseCaches(false);
 
-		    OutputStreamWriter writer = new OutputStreamWriter( con.getOutputStream() );
-		    writer.write( requestXml );
-		    writer.flush();
-		    writer.close();
+			// tell the web server what we are sending
+			con.setRequestProperty("Content-Type", "application/xml");
 
-		    // reading the response
-		    InputStreamReader reader = new InputStreamReader( con.getInputStream() );
+			OutputStreamWriter writer = new OutputStreamWriter(
+					con.getOutputStream());
+			writer.write(requestXml);
+			writer.flush();
+			writer.close();
 
-		    StringBuilder buf = new StringBuilder();
-		    char[] cbuf = new char[ 2048 ];
-		    int num;
+			// reading the response
+			InputStreamReader reader = new InputStreamReader(
+					con.getInputStream());
 
-		    while ( -1 != (num=reader.read( cbuf )))
-		    {
-		        buf.append( cbuf, 0, num );
-		    }
+			StringBuilder buf = new StringBuilder();
+			char[] cbuf = new char[2048];
+			int num;
 
-		    String result = buf.toString();
-		    System.out.println( "\nResponse from server after POST:\n" + result );
-		    
-		    return true;
-		    
-		}
-		catch( Throwable t )
-		{
-			System.out.println("Exception while uploading to DHIS: "+t);
-		    return false;
+			while (-1 != (num = reader.read(cbuf))) {
+				buf.append(cbuf, 0, num);
+			}
+
+			String result = buf.toString();
+
+			if (result.indexOf("SMS success") != -1) {
+				logger.info("SMS Success response from server (may not mean it worked)");
+			} else {
+				logger.info("SMS negative response from server (may not mean it didn't work)");
+			}
+			// logger.debug("Response from server after POST:\n" + result);
+
+			return true;
+
+		} catch (Throwable t) {
+			logger.info("Exception while uploading to DHIS: " + t);
+			return false;
 		}
 	}
-	
-	public boolean forwardRequest(String queryString)
-	{
-		return forwardRequest(dhisUrl, username, password, queryString);	 
+
+	public boolean forwardRequest(String queryString) {
+
+		// testIt();
+
+		if (dhisUrl.startsWith("https")) {
+			return forwardHTTPSRequest(dhisUrl, username, password, queryString);
+		} else {
+			return forwardRequest(dhisUrl, username, password, queryString);
+		}
 	}
-	
-	public boolean forwardRequest(String urlString, String u, String p, String queryString)
-	{
-		try
-		{
+
+	public boolean forwardHTTPSRequest(String urlString, String u, String p,
+			String queryString) {
+
+		String https_url = urlString + '?' + queryString;
+		logger.info("HTTPS url:" + https_url);
+		
+		URL url;
+		try {
+
+			url = new URL(https_url);
+			HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
 			
 			String authStr = u + ":" + p;
-	        String authEncoded = Base64.encodeBytes(authStr.getBytes());
+			String authEncoded = Base64.encodeBytes(authStr.getBytes());
+			con.setRequestProperty("Authorization", "Basic " + authEncoded);
 
-		    URL url = new URL( urlString+"?"+queryString );
-	        //URL url = new URL( dhisUrl );
-		    URLConnection con = url.openConnection();
-	        con.setRequestProperty("Authorization", "Basic " + authEncoded);
-		    
-		    // specify that we will send output and accept input
-		    con.setDoInput(true);
-		    con.setDoOutput(true);
+			con.setRequestMethod("GET");
+			
+			con.setConnectTimeout(20000); // long timeout, but not infinite
+			con.setReadTimeout(20000);
 
-		    con.setConnectTimeout( 20000 );  // long timeout, but not infinite
-		    con.setReadTimeout( 20000 );
+			con.setUseCaches(false);
+			con.setDefaultUseCaches(false);
 
-		    con.setUseCaches (false);
-		    con.setDefaultUseCaches (false);
+			// tell the web server what we are sending
+			con.setRequestProperty("Content-Type",
+					"application/x-www-form-urlencoded");
 
-		    // tell the web server what we are sending
-		    con.setRequestProperty ( "Content-Type", "application/x-www-form-urlencoded" );
+			con.setDoInput(true);
+			con.setDoOutput(true);
+			con.connect();
+						
+			//logger.info("Response Code : " + con.getResponseCode());
+			//logger.info("Cipher Suite : " + con.getCipherSuite());
+			//logger.info("\n");
+			
+			//con.setDoInput(true);
+			//con.setDoOutput(true);
+			
+			OutputStreamWriter writer = new OutputStreamWriter(
+					con.getOutputStream());
+			// writer.write( "?"+queryString );
+			writer.write("");
+			writer.flush();
+			writer.close();
 
-		    OutputStreamWriter writer = new OutputStreamWriter( con.getOutputStream() );
-		    //writer.write( "?"+queryString );
-		    writer.write( "" );
-		    writer.flush();
-		    writer.close();
+			// reading the response
+			InputStreamReader reader = new InputStreamReader(
+					con.getInputStream());
 
-		    // reading the response
-		    InputStreamReader reader = new InputStreamReader( con.getInputStream() );
+			StringBuilder buf = new StringBuilder();
+			char[] cbuf = new char[2048];
+			int num;
 
-		    StringBuilder buf = new StringBuilder();
-		    char[] cbuf = new char[ 2048 ];
-		    int num;
+			while (-1 != (num = reader.read(cbuf))) {
+				buf.append(cbuf, 0, num);
+			}
 
-		    while ( -1 != (num=reader.read( cbuf )))
-		    {
-		        buf.append( cbuf, 0, num );
-		    }
+			String result = buf.toString();
 
-		    String result = buf.toString();
-		    System.out.println( "\nResponse from server after POST:\n" + result );
-		    System.out.println("\nServer url:"+urlString);
-		    
-		    return true;
-		    
+			if (result.indexOf("SMS success") != -1) {
+				logger.info("SMS Success response from server (may not mean it worked) "+con.getResponseCode());
+			} else {
+				logger.info("SMS negative response from server (may not mean it didn't work) "+con.getResponseCode());
+			}
+			logger.debug("Response from server after POST:\n" + result);
+			logger.debug("Server url:" + urlString);
+
+			return true;
+
+			// dump all the content
+			// print_content(con);
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		catch( Throwable t )
-		{
-			System.out.println("Exception while uploading to DHIS: "+t);
-		    return false;
+
+		return true;
+	}
+
+	public boolean forwardRequest(String urlString, String u, String p,
+			String queryString) {
+		try {
+
+			String authStr = u + ":" + p;
+			String authEncoded = Base64.encodeBytes(authStr.getBytes());
+
+			URL url = new URL(urlString + "?" + queryString);
+			// URL url = new URL( dhisUrl );
+			URLConnection con = url.openConnection();
+			con.setRequestProperty("Authorization", "Basic " + authEncoded);
+
+			// specify that we will send output and accept input
+			con.setDoInput(true);
+			con.setDoOutput(true);
+
+			con.setConnectTimeout(20000); // long timeout, but not infinite
+			con.setReadTimeout(20000);
+
+			con.setUseCaches(false);
+			con.setDefaultUseCaches(false);
+
+			// tell the web server what we are sending
+			con.setRequestProperty("Content-Type",
+					"application/x-www-form-urlencoded");
+
+			OutputStreamWriter writer = new OutputStreamWriter(
+					con.getOutputStream());
+			// writer.write( "?"+queryString );
+			writer.write("");
+			writer.flush();
+			writer.close();
+
+			// reading the response
+			InputStreamReader reader = new InputStreamReader(
+					con.getInputStream());
+
+			StringBuilder buf = new StringBuilder();
+			char[] cbuf = new char[2048];
+			int num;
+
+			while (-1 != (num = reader.read(cbuf))) {
+				buf.append(cbuf, 0, num);
+			}
+
+			String result = buf.toString();
+
+			if (result.indexOf("SMS success") != -1) {
+				logger.info("SMS Success response from server (may not mean it worked)");
+			} else {
+				logger.info("SMS negative response from server (may not mean it didn't work)");
+			}
+			logger.debug("Response from server after POST:\n" + result);
+			logger.debug("Server url:" + urlString);
+
+			return true;
+
+		} catch (Throwable t) {
+			logger.error("Exception while uploading to DHIS: " + t);
+			return false;
 		}
 	}
-	
+
+	static {
+		HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+			public boolean verify(String hostname, SSLSession session) {
+				// ip address of the service URL(like.23.28.244.244)
+//				if (hostname.equals("88.80.189.129"))
+//					return true;
+//				return false;
+				return true;
+			}
+
+		});
+	}
+
+	private void print_https_cert(HttpsURLConnection con) {
+
+		if (con != null) {
+
+			try {
+
+				logger.info("Response Code : " + con.getResponseCode());
+				logger.info("Cipher Suite : " + con.getCipherSuite());
+				logger.info("\n");
+
+				Certificate[] certs = con.getServerCertificates();
+				for (Certificate cert : certs) {
+					logger.info("Cert Type : " + cert.getType());
+					logger.info("Cert Hash Code : " + cert.hashCode());
+					logger.info("Cert Public Key Algorithm : "
+							+ cert.getPublicKey().getAlgorithm());
+					logger.info("Cert Public Key Format : "
+							+ cert.getPublicKey().getFormat());
+					logger.info("\n");
+				}
+
+			} catch (SSLPeerUnverifiedException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	private void print_content(HttpsURLConnection con) {
+		if (con != null) {
+
+			try {
+
+				logger.info("****** Content of the URL ********");
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						con.getInputStream()));
+
+				String input;
+
+				while ((input = br.readLine()) != null) {
+					System.out.println(input);
+				}
+				br.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
 
 }
--- dhis-sms-in/src/main/webapp/index.jsp
+++ dhis-sms-in/src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <head><title>Hello :: Spring Application</title></head>
 <body>
-	<h1>Index.jsp - Spring Application</h1>
-	<p>Greetings.</p>
+	<h1>HTTP SMS Gateway for DHIS2</h1>
+	<p>This gateway is not an official part of the DHIS2 source code. Please refer to <a href="http://dhis2.org/">dhis.org</a> for information and documentation about DHIS2.</a></p>
 </body>
